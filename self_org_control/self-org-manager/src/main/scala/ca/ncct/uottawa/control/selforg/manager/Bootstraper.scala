d8a312cab4a372918984fa18f0a8fd000da4a4b3
bogsolomon <bog.solomon@gmail.com>
2016-07-14 21:46:23 -0400

    Reading IPv$ address of eth0 for command

--- self_org_control/self-org-manager/src/main/scala/ca/ncct/uottawa/control/selforg/manager/Bootstraper.scala
+++ self_org_control/self-org-manager/src/main/scala/ca/ncct/uottawa/control/selforg/manager/Bootstraper.scala
@@ -1,6 +1,7 @@
 package ca.ncct.uottawa.control.selforg.manager
 
 import java.net.{NetworkInterface, InetAddress}
+import java.util
 
 import scala.xml.XML
 import akka.actor.{Props, ActorSystem}
@@ -12,20 +13,31 @@ import ca.ncct.uottawa.control.selforg.manager.config.Config
 object Bootstraper {
 
   val system = ActorSystem("controlSystem")
-  val startCount = 0
+  var startCount = 0
 
   def startManager(config: Config): Unit = {
     val interfaces = NetworkInterface.getNetworkInterfaces
-    var localIpAddress: String = ""
-    while (interfaces.hasMoreElements) {
-      val element = interfaces.nextElement
-      if (element.getDisplayName.equalsIgnoreCase("eth0")) {
-        localIpAddress = element.getInetAddresses.nextElement().getHostAddress
+    def findEth0Address: String = {
+      while (interfaces.hasMoreElements) {
+        val element = interfaces.nextElement
+        if (element.getDisplayName.equalsIgnoreCase("eth0")) {
+          val addresses: util.Enumeration[InetAddress] = element.getInetAddresses
+          while (addresses.hasMoreElements) {
+            val address = addresses.nextElement
+            if (!address.getHostAddress.contains(":")) {
+              return address.getHostAddress
+            }
+          }
+        }
       }
+      ""
     }
+
+    val localIpAddress: String = findEth0Address
     val port = config.startPort + startCount
-    println("Command is: " + "docker run -d --net=" + config.networkName + " --name=red5-" + startCount + " -e \"red5_port=" + port +
-      "\" -e \"red5_ip="+localIpAddress+"\" -p " + port + ":" + config.startPort + " bsolomon/red5-media:v1")
+    val command = "docker run -d --net=" + config.networkName + " --name=red5-" + startCount + " -e \"red5_port=" + port +
+      "\" -e \"red5_ip="+localIpAddress+"\" -p " + port + ":" + config.startPort + " bsolomon/red5-media:v1"
+    println("Base command is: " + command)
   }
 
   def main(args: Array[String]): Unit = {
